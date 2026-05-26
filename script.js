98c2706db58067fe42b00dc5daa48832a5c61676
madush26 <163010996+madush26@users.noreply.github.com>
2026-05-26 12:53:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
 function openCard(){
     document.getElementById("cover").style.display = "none";
     document.getElementById("card").style.display = "flex";
-
-    document.getElementById("music").play();
 }
 
 // Countdown
 const weddingDate = new Date("July 4, 2026 11:00:00").getTime();
 
-const x = setInterval(function() {
-
+setInterval(() => {
     let now = new Date().getTime();
     let distance = weddingDate - now;
 
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
+    document.getElementById("days").innerText = Math.floor(distance / (1000*60*60*24));
+    document.getElementById("hours").innerText = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
+    document.getElementById("minutes").innerText = Math.floor((distance % (1000*60*60)) / (1000*60));
+    document.getElementById("seconds").innerText = Math.floor((distance % (1000*60)) / 1000);
 
 }, 1000);
