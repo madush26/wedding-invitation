02c20ad6670afee41f9c0bd7d39fb73d3f3b2bb7
madush26 <163010996+madush26@users.noreply.github.com>
2026-05-26 12:55:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 function openCard(){
-    document.getElementById("cover").style.display = "none";
-    document.getElementById("card").style.display = "flex";
+
+    document.getElementById("cover").style.top = "-100%";
+
+    setTimeout(() => {
+        document.getElementById("card").style.opacity = "1";
+    }, 800);
+
+    // Countdown
+    let weddingDate = new Date("July 04, 2026 11:00:00").getTime();
+
+    setInterval(function(){
+
+        let now = new Date().getTime();
+        let distance = weddingDate - now;
+
+        let days = Math.floor(distance / (1000*60*60*24));
+        let hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+        let minutes = Math.floor((distance%(1000*60*60))/(1000*60));
+        let seconds = Math.floor((distance%(1000*60))/1000);
+
+        document.getElementById("days").innerHTML = days;
+        document.getElementById("hours").innerHTML = hours;
+        document.getElementById("minutes").innerHTML = minutes;
+        document.getElementById("seconds").innerHTML = seconds;
+
+        if(distance < 0){
+            document.getElementById("timer").innerHTML = "💍 The Big Day Has Arrived!";
+        }
+
+    },1000);
 }
-
-// Countdown
-const weddingDate = new Date("July 4, 2026 11:00:00").getTime();
-
-setInterval(() => {
-    let now = new Date().getTime();
-    let distance = weddingDate - now;
-
-    document.getElementById("days").innerText = Math.floor(distance / (1000*60*60*24));
-    document.getElementById("hours").innerText = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
-    document.getElementById("minutes").innerText = Math.floor((distance % (1000*60*60)) / (1000*60));
-    document.getElementById("seconds").innerText = Math.floor((distance % (1000*60)) / 1000);
-
-}, 1000);
