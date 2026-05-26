f301a90b238dfbb8747e1d960d83af3ec3c969b6
madush26 <163010996+madush26@users.noreply.github.com>
2026-05-26 12:22:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function openCard(){
+
+    let cover = document.getElementById("cover");
+    let card = document.getElementById("card");
+
+    cover.style.top = "-100%";
+
+    setTimeout(() => {
+        card.style.opacity = "1";
+    }, 800);
+
+    // 🎯 Set Wedding Date (CHANGE THIS DATE)
+let weddingDate = new Date("July 04, 2026 11:00:00").getTime();
+
+let countdown = setInterval(function() {
+
+    let now = new Date().getTime();
+    let distance = weddingDate - now;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (distance < 0) {
+        clearInterval(countdown);
+        document.getElementById("timer").innerHTML = "💍 The Big Day Has Arrived!";
+    }
+
+}, 1000);
+
+}
