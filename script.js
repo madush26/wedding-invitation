383e8457fead776126ec18a740e179dd6b1e210c
madush26 <163010996+madush26@users.noreply.github.com>
2026-05-26 12:50:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
 function openCard(){
+    document.getElementById("cover").style.display = "none";
+    document.getElementById("card").style.display = "flex";
 
-    document.getElementById("cover").style.top = "-100%";
-
-    setTimeout(() => {
-        document.getElementById("card").style.opacity = "1";
-    }, 800);
-
-    // Countdown
-    let weddingDate = new Date("July 04, 2026 11:00:00").getTime();
-
-    setInterval(function(){
-
-        let now = new Date().getTime();
-        let distance = weddingDate - now;
-
-        let days = Math.floor(distance / (1000*60*60*24));
-        let hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
-        let minutes = Math.floor((distance%(1000*60*60))/(1000*60));
-        let seconds = Math.floor((distance%(1000*60))/1000);
-
-        document.getElementById("days").innerHTML = days;
-        document.getElementById("hours").innerHTML = hours;
-        document.getElementById("minutes").innerHTML = minutes;
-        document.getElementById("seconds").innerHTML = seconds;
-
-        if(distance < 0){
-            document.getElementById("timer").innerHTML = "💍 The Big Day Has Arrived!";
-        }
-
-    },1000);
+    document.getElementById("music").play();
 }
+
+// Countdown
+const weddingDate = new Date("July 4, 2026 11:00:00").getTime();
+
+const x = setInterval(function() {
+
+    let now = new Date().getTime();
+    let distance = weddingDate - now;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+}, 1000);
